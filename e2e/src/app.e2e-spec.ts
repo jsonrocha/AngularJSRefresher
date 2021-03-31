408589ad06f02388b82b6eaddb14517b12df6c14
Jason Rocha <jrocha@proforma.com>
2021-03-31 15:53:18 -0400

    Test Fix Please enter the commit message for your changes. Lines starting

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('firstApp app is running!');
+    expect(await page.getTitleText()).toEqual('AngularJSRefresher app is running!');
   });
 
   afterEach(async () => {
